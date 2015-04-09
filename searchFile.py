abfe171a2e8c49219d146fe477774b3b06451078
Daniel MacLennan <dsmaclen@umich.edu>
2015-04-09 13:35:18 -0400

    SSL keys

--- searchFile.py
+++ searchFile.py
@@ -3,30 +3,54 @@
 import sys
 import re
 import urllib2
+import sqlite3
 
 def downloadURL(url):
     r = urllib2.urlopen(url)
     return r.read()
 
+
+
+def isSSLKey(text):
+    privateKeyRE = re.compile(r'-----BEGIN (RSA )?PRIVATE KEY-----\n([0-9A-Za-z=+/\n]{100,})-----END (RSA )?PRIVATE KEY-----')
+    return re.match(privateKeyRE, text) is not None
+
+def isBitcoin(url):
+    return url.find("wallet.dat") >= 0
+
 def extractAWSKeys(text):
     keys = []
 
-    awsRE = re.compile('aws', re.IGNORECASE)
+    keynameRE = re.compile(r'(AKA)')
     keyRE = re.compile(r'([^0-9A-Za-z+/]|^)([0-9A-Za-z=+/]{40})([^0-9A-Za-z=+/]|$)')
 
-    if re.search(awsRE, text):
+    if re.search(keynameRE, text):
         keys += [m[1] for m in re.findall(keyRE, text)]
     return keys
 
-def getKeysFromURL(url):
+
+def findSecrets(url, dbc):
     text = downloadURL(url)
+
+    # Check for various kinds of secrets and save to NoSQL
     keys = extractAWSKeys(text)
-    return (text, keys)
+    if keys:
+        dbc.execute('INSERT INTO Secrets VALUES("");')
+
 
 def main():
-    keyResults = [getKeysFromURL(url) for url in sys.stdin]
-    for text, keys in keyResults:
-        print repr(keys)
+    #keyResults = [getKeysFromURL(url) for url in sys.stdin]
+    #for text, keys in keyResults:
+    #    print repr(keys)
+
+    fileName = raw_input("file name: ")
+    with open(fileName, 'r') as f:
+        s = f.read()
+        if re.match(privateKeyRE, s):
+            print "Match!"
+        else:
+            print "Not a match..."
+        
 
 if __name__=='__main__':
     main()
